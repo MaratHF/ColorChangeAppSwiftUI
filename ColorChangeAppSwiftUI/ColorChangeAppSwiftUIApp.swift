5f40455291098ca14a538c278364a9a2be698276
Marat Khayrullin <m.f.khayrullin@gmail.com>
2022-08-11 23:49:54 +0300

    Initial Commit

---
+++ ColorChangeAppSwiftUI/ColorChangeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangeAppSwiftUIApp.swift
+//  ColorChangeAppSwiftUI
+//
+//  Created by MAC  on 11.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
